21115e3333ed34260bb5057c42dbe198a2ce019e
Nicolas Palermo <h4ckcheek@gmail.com>
2024-02-22 15:11:56 -0300

    init

---
+++ web3provdiers/rust/target/debug/deps/rust-4604edc293b6b8f3.d
@@ -0,0 +1,5 @@
+/Users/nico/work/freewillai/benchmark/web3provdiers/rust/target/debug/deps/rust-4604edc293b6b8f3: src/main.rs
+
+/Users/nico/work/freewillai/benchmark/web3provdiers/rust/target/debug/deps/rust-4604edc293b6b8f3.d: src/main.rs
+
+src/main.rs:
---
+++ web3provdiers/rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+/Users/nico/work/freewillai/benchmark/web3provdiers/rust/target/debug/rust: /Users/nico/work/freewillai/benchmark/web3provdiers/rust/src/main.rs
